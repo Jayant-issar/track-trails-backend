91800649917db6eadac35f32e8c48f1396e2d51a
JAYANT ISSAR <jayantissar23@gmail.com>
2025-01-30 12:30:18 +0530

    added test route

--- src/user/userRouter.ts
+++ src/user/userRouter.ts
@@ -1,9 +1,11 @@
 import { Hono } from 'hono'
 import onBoardUserApp from './onBoardUser'
+import testApp from './controlller/testapis'
 
 const userRouter = new Hono()
 
 userRouter.route('/onboard', onBoardUserApp)    
+userRouter.route('/test', testApp)
 userRouter.get('/', async (c) => {
     return c.text(' welcome to the user router')
 })
