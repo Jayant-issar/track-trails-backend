ccd9bb107544b6f47d7a2e7da139ed04df39b49b
JAYANT ISSAR <jayantissar23@gmail.com>
2025-01-31 18:55:21 +0530

    test api added

--- src/user/controlller/testapis.ts
+++ src/user/controlller/testapis.ts
@@ -1,29 +1,34 @@
 import { Hono } from "hono";
 import { authMiddleware } from "../middlewares/authMiddlware";
 import { createClerkClient } from "@clerk/backend";
+import { createApplicationController } from "./applicationTrack";
 
 const testApp = new Hono();
-testApp.get('/', authMiddleware, async (c) => {
-    const sessionClaims = c.get('sessionClaims');
+testApp.get('/', authMiddleware,createApplicationController
     
-    const clerkClient = createClerkClient({
-        secretKey: c.env.CLERK_SECRET_KEY,
-        publishableKey: c.env.CLERK_PUBLISHABLE_KEY,
-    });
+    // async (c) => {
+    // const sessionClaims = c.get('sessionClaims');
+    
+    // const clerkClient = createClerkClient({
+    //     secretKey: c.env.CLERK_SECRET_KEY,
+    //     publishableKey: c.env.CLERK_PUBLISHABLE_KEY,
+    // });
 
-    try {
-        const user = await clerkClient.users.getUser(sessionClaims.userId);
+    // try {
+    //     const user = await clerkClient.users.getUser(sessionClaims.userId);
         
-        return c.json({
-            id: user.id,
-            email: user.emailAddresses[0]?.emailAddress,
-            firstName: user.firstName,
-            lastName: user.lastName,
-            imageUrl: user.imageUrl
-        });
-    } catch (error) {
-        return c.json({ error: "Failed to fetch user data" }, 500);
-    }
-});
+    //     return c.json({
+    //         id: user.id,
+    //         email: user.emailAddresses[0]?.emailAddress,
+    //         firstName: user.firstName,
+    //         lastName: user.lastName,
+    //         imageUrl: user.imageUrl
+    //     });
+    // } catch (error) {
+    //     return c.json({ error: "Failed to fetch user data" }, 500);
+    // }
+    //}
+);
+testApp.get('/createApplication',authMiddleware,createApplicationController)
 
 export default testApp;
