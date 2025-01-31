b17da890ff7da102146c477d334647db127fd257
JAYANT ISSAR <jayantissar23@gmail.com>
2025-01-31 18:50:56 +0530

    added route redirecting to the application user route

--- src/user/userRouter.ts
+++ src/user/userRouter.ts
@@ -1,11 +1,13 @@
 import { Hono } from 'hono'
 import onBoardUserApp from './onBoardUser'
 import testApp from './controlller/testapis'
+import { applicationTrackerRouteApp } from './applicationTrackerRoute'
 
 const userRouter = new Hono()
 
 userRouter.route('/onboard', onBoardUserApp)    
 userRouter.route('/test', testApp)
+userRouter.route('/application', applicationTrackerRouteApp)
 userRouter.get('/', async (c) => {
     return c.text(' welcome to the user router')
 })
