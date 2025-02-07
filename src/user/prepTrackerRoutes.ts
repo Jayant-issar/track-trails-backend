e3d8736f228ea2e4f4fd7bcee682aadc4499d5e1
JAYANT ISSAR <jayantissar23@gmail.com>
2025-02-07 12:46:35 +0530

    added prep tracker route

--- src/user/prepTrackerRoutes.ts
+++ src/user/prepTrackerRoutes.ts
@@ -1,12 +1,55 @@
 import { Hono } from "hono";
 import { authMiddleware } from "./middlewares/authMiddlware";
 import { validatePrepMetricInput } from "./middlewares/prepTracker";
-import { createPrepMetricController } from "./controlller/prepTracker";
+import { createPrepMetricController, deleteMetricController, getAllUserMetricsController, getDailyMetricController, updateDailyProgressController } from "./controlller/prepTracker";
+import { db } from "../utils/prisma";
 
 
 export const prepTrackerApp = new Hono();
 
 prepTrackerApp.post("/create", authMiddleware, validatePrepMetricInput, createPrepMetricController)
+
+prepTrackerApp.get("/getall/:clerkId",async (c)=>{
+    const clerkId = c.req.param("clerkId");
+    try {
+        const allPrepMetrics = await db.preparationMetric.findMany({
+            where:{
+                user:{
+                    clerkId
+                }
+            },
+            include:{
+                progress:true,
+                targetPerDay:true,
+                user:true,
+            }
+        });
+
+        return c.json({
+            message:"All prep metrics fetched successfully",
+            data:allPrepMetrics,
+            success:true
+        },200)
+    } catch (error) {
+        return c.json({
+            message:"Failed to fetch prep metrics",
+            success:false
+        },500)  
+    }
+})
+
+prepTrackerApp.get('/metrics/daily', getDailyMetricController);
+prepTrackerApp.get('/metrics', getAllUserMetricsController);
+prepTrackerApp.patch('/metrics/progress', updateDailyProgressController);
+prepTrackerApp.delete('/metrics', deleteMetricController);
+
+
+
+
+
+
+
+
 
 prepTrackerApp.get("/",(c)=>{
     return c.json({
