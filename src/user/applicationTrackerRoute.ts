88fa8f238e78244414520f222d80471fb3a694c2
JAYANT ISSAR <jayantissar23@gmail.com>
2025-02-27 17:30:34 +0530

    added patch route

--- src/user/applicationTrackerRoute.ts
+++ src/user/applicationTrackerRoute.ts
@@ -1,9 +1,11 @@
 import { Hono } from "hono";
-import { createApplicationController, getApplicationsController } from "./controlller/applicationTrack";
+import { createApplicationController, getApplicationsController, updateApplicationStatusController } from "./controlller/applicationTrack";
 import { authMiddleware } from "./middlewares/authMiddlware";
 import { validateApplication } from "./middlewares/inputValidation/applicationTrackerInputValidator";
+import { validateRequest } from "./middlewares/inputValidation/inputValidationMiddleware";
+import { updateApplicationStatusSchema } from "./schemas/applicationSchema";
 export const applicationTrackerRouteApp = new Hono();
 
 applicationTrackerRouteApp.post('/', authMiddleware,validateApplication ,createApplicationController)
-
+applicationTrackerRouteApp.patch("/", authMiddleware,validateRequest(updateApplicationStatusSchema), updateApplicationStatusController)
 applicationTrackerRouteApp.get("/",authMiddleware,getApplicationsController)
