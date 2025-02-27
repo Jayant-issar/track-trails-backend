dc1edf4857d4ce8294a7135b83576723cee7ed31
JAYANT ISSAR <jayantissar23@gmail.com>
2025-02-27 17:34:04 +0530

    added update

--- src/coldOutReachRoute.ts
+++ src/coldOutReachRoute.ts
@@ -1,12 +1,15 @@
 import { Hono } from "hono";
 import { authMiddleware } from "./user/middlewares/authMiddlware";
 import { validateColdOutReachInput } from "./user/middlewares/inputValidation/coldOutReachInputValidation";
-import { createColdOutReachController, getColdOutReachController } from "./user/controlller/coldOutreachContoller";
+import { createColdOutReachController, getColdOutReachController, updatecoldoutreachController } from "./user/controlller/coldOutreachContoller";
+import { validateRequest } from "./user/middlewares/inputValidation/inputValidationMiddleware";
+import { updateaColdOutReachSchema } from "./user/schemas/coldOutreachSchemas";
 
 const coldOutReachApp = new Hono();
 
 coldOutReachApp.post("/create", authMiddleware,validateColdOutReachInput,createColdOutReachController)
 coldOutReachApp.get("/",authMiddleware,getColdOutReachController)
+coldOutReachApp.patch("/update",authMiddleware, validateRequest(updateaColdOutReachSchema),updatecoldoutreachController)
 
 
 export default coldOutReachApp;
